54b1080d4cd972fd807ec812540c8b2e4a0b8aea
Luis Weir <luisweir@Luiss-MacBook-Pro.local>
2019-09-20 18:54:11 +0100

    updated to work with Winston 3.x, lodash 4.x and Node 12.x

--- example/log.js
+++ example/log.js
@@ -1,16 +1,32 @@
-var winston = require('winston');
-var winstonRemoteTransport = require('../index').Transport;
+const winston = require('winston');
 
-var logger = new (winston.Logger)({
+const colors = { 
+    error: 'bold white redBG',
+    warn: 'italic black yellowBG',
+    info: 'italic white blueBG',
+    http: 'dim green blackBG',
+    verbose: 'italic white blackBG',
+    debug: 'bold white cyanBG',
+    silly: 'underline white blackBG'
+};
+
+winston.addColors(colors);
+
+const logger = winston.createLogger({
+    level: "silly",
     transports: [
-        new (winston.transports.Console)(),
-        new (winstonRemoteTransport)({
+        new winston.transports.Console(),
+        new winston.transports.Http({
             host: '127.0.0.1',
-            port: 9003,
-            label: 'Client',
-            stack: true
+            port: 9003
         })
     ]
 });
 
+logger.error('foo');
+logger.warn('foo');
 logger.info('foo');
+logger.http('foo');
+logger.verbose('foo');
+logger.debug('foo');
+logger.silly('foo');
--- example/server.js
+++ example/server.js
@@ -2,14 +2,37 @@ var winstonRemote = require('../index').Server;
 var winston = require('winston');
 
 var winstonServer = winstonRemote.createServer({
-    port: 9003
+  host: '127.0.0.1',
+  port: 9003
+});
+
+const colors = { 
+  error: 'bold white redBG',
+  warn: 'italic black yellowBG',
+  info: 'italic white blueBG',
+  http: 'dim green blackBG',
+  verbose: 'italic white blackBG',
+  debug: 'bold white cyanBG',
+  silly: 'underline white blackBG'
+};
+
+winston.addColors(colors);
+
+winstonServer.logger = winston.createLogger({
+  level: "silly",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    winston.format.splat(),
+    winston.format.simple(),
+    winston.format.printf(
+      msg => `[${msg.timestamp}] (${msg.level}): ${msg.message}`
+    )
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: [__dirname, '/info.log'].join('') })
+  ]
 });
 
 winstonServer.listen();
-
-winstonServer.logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)(),
-        new winston.transports.File({ filename: [__dirname,'/info.log'].join('') })
-    ]
-});
